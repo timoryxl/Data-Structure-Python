0a3a20279e58de5e40f34ae9c2701e046ad9d019
timoryxl <tim-xl@hotmail.com>
2018-12-17 18:05:31 -0800

    Josephus problem

--- linkedlist/circular.py
+++ linkedlist/circular.py
@@ -41,7 +41,7 @@ class CircularLinkedList:
             if cur == self.head:
                 break
 
-    def remove_node(self, key):
+    def remove(self, key):
         if self.head is None:
             print('error')
             return
@@ -61,13 +61,55 @@ class CircularLinkedList:
                     prev.next = cur.next
                     cur = cur.next
 
+    def remove_node(self, node):
+        if self.head is None:
+            print('error')
+            return
+        elif self.head == node:
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = self.head.next
+            self.head = self.head.next
+        else:
+            cur = self.head
+            prev = None
+            while cur.next != self.head:
+                prev = cur
+                cur = cur.next
+                if cur == node:
+                    prev.next = cur.next
+                    cur = cur.next
+
+    def josephus_circle(self, step):
+        cur = self.head
+        length = 0
+        while cur.next != self.head:
+            cur = cur.next
+            length += 1
+        length += 1
+        cur = self.head
+        while length > 1:
+            count = 1
+            while count != step:
+                count += 1
+                cur = cur.next
+            self.remove_node(cur)
+            cur = cur.next
+            length -= 1
+
 
 if __name__ == '__main__':
     cllist = CircularLinkedList()
-    cllist.append('C')
-    cllist.append('B')
-    cllist.prepend('B')
-    cllist.prepend('A')
-    cllist.remove_node('D')
-    cllist.remove_node('B')
+    # cllist.append('C')
+    # cllist.append('B')
+    # cllist.prepend('B')
+    # cllist.prepend('A')
+    # cllist.remove_node('D')
+    # cllist.remove_node('B')
+    cllist.append(1)
+    cllist.append(2)
+    cllist.append(3)
+    cllist.append(4)
+    cllist.josephus_circle(2)
     cllist.print_list()
