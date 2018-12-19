883103e2f2a43d548a3074caaa1d04597a854203
timoryxl <tim-xl@hotmail.com>
2018-12-18 19:58:34 -0800

    Add after/before node

--- linkedlist/doubly.py
+++ linkedlist/doubly.py
@@ -35,15 +35,48 @@ class DoublyLinkedList:
             print(cur.data)
             cur = cur.next
 
-    def add_after_node(self, data, key):
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                cur.next.prev = new_node
+                new_node.next = cur.next
+                cur.next = new_node
+                new_node.prev = cur
+                return
+            cur = cur.next
+
+    def add_before_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                cur.prev.next = new_node
+                new_node.prev = cur.prev
+                new_node.next = cur
+                cur.prev = new_node
+                return
+            cur = cur.next
 
 
 if __name__ == '__main__':
     dllist = DoublyLinkedList()
     dllist.prepend(1)
     dllist.append(2)
-    dllist.prepend(7)
+    # dllist.prepend(7)
     dllist.append(3)
     dllist.append(4)
     dllist.append(5)
+    # dllist.add_after_node(1, 11)
+    # dllist.add_after_node(2, 12)
+    # dllist.add_after_node(4, 14)
+    dllist.add_before_node(2, 11)
+    dllist.add_before_node(1, 11)
     dllist.print_list()
