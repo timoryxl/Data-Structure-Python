d446aa8e8d783838ba063a1d137dcb58e0935fd0
timoryxl <tim-xl@hotmail.com>
2018-12-28 18:28:00 -0800

    Level order traversal using queue

--- binarytree/binarytree.py
+++ binarytree/binarytree.py
@@ -5,6 +5,30 @@ class Node:
         self.right = None
 
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            val = self.items[0]
+            del self.items[0]
+            return val
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0].value
+
+    def __len__(self):
+        return len(self.items)
+
+
 class BinaryTree:
     def __init__(self, root):
         self.root = Node(root)
@@ -12,6 +36,12 @@ class BinaryTree:
     def print_tree(self, traversal_type):
         if traversal_type == 'preorder':
             return self.preorder_print(tree.root, '')
+        elif traversal_type == 'inorder':
+            return self.inorder_print(tree.root, '')
+        elif traversal_type == 'postorder':
+            return self.postorder_print(tree.root, '')
+        elif traversal_type == 'levelorder':
+            return self.levelorder_print(tree.root, '')
         else:
             print('Not Supported')
             return
@@ -37,6 +67,20 @@ class BinaryTree:
             traversal = self.postorder_print(start.right, traversal)
         return traversal
 
+    def levelorder_print(self, start, traversal):
+        if start is None:
+            return
+        queue = Queue()
+        queue.enqueue(start)
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + '-'
+            node = queue.dequeue()
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        return traversal
+
 
 if __name__ == '__main__':
     tree = BinaryTree(1)
@@ -46,4 +90,5 @@ if __name__ == '__main__':
     tree.root.left.right = Node(5)
     tree.root.right.left = Node(6)
     tree.root.right.right = Node(7)
-    print(tree.print_tree('preorder'))
+    # print(tree.print_tree('preorder'))
+    print(tree.print_tree('levelorder'))
